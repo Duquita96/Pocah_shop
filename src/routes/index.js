4dfc6d01ba2ee53a9164b5cc834779182da6f6f1
Claudia <duquita96@gmail.com>
2024-03-22 14:40:56 +0100

    adde new path (register and login)

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,5 +8,10 @@ router.get('/about', (req, res) => res.render('about', {title: 'About'}))
 
 router.get('/products', (req, res) => res.render('products', {title: 'Products'}))
 
+router.get('/register', (req, res) => res.render('register', {title: 'Register'}))
+
+router.get('/login', (req, res) => res.render('login', {title: 'Log in'}))
+
+
 
 export default router;
